0acd25f5f2833d5ddc977107ca26229cec13c808
Eikooc Sun <cookie@cookiepower.tech>
2025-05-24 21:14:39 +0800

    Add collectJar task to aggregate JAR files and update release.yml for simplified asset upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,3 +19,32 @@ subprojects {
         useJUnitPlatform()
     }
 }
+
+tasks.register<Copy>("collectJar") {
+    val pluginsProject = listOf(
+        project(":economyb-spigot"),
+        project(":economyb-spigot-api"),
+        project(":economyb-spigot-api2vault"),
+    )
+
+    dependsOn(pluginsProject.map { it.tasks.named("jar") })
+
+    val libsDir  = layout.buildDirectory.dir("libs").get().asFile
+    val tmpDir = layout.buildDirectory.dir("tmp").get().asFile
+
+    doFirst {
+        if (libsDir.exists()) { libsDir.deleteRecursively() }
+        if (tmpDir.exists()) { tmpDir.deleteRecursively() }
+        println("Clean up root project build directories")
+    }
+
+    from(pluginsProject.map { it.layout.buildDirectory.dir("libs") }) {
+        include("*.jar")
+    }
+
+    into(libsDir )
+}
+
+tasks.named("build") {
+    dependsOn("collectJar")
+}
